3a3cd72335d127d4c13b8a5b5da159daa9f8d035
paco <dimepaco@gmail>
2017-11-11 13:51:44 +0100

    * CHANGED: nombre fichero

--- websocket-min/src/main/java/es/AnnotatedEndPoint.java
+++ websocket-min/src/main/java/es/AnnotatedEndPoint.java
@@ -1,13 +1,30 @@
 package es;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/mensaje")
-public class MiWebSocket {
+@ServerEndpoint("/endpoint1")
+public class AnnotatedEndPoint {
 
-    @OnMessage
-    public String mensaje(String mensaje) {
-        return "hola desde el servidor el mensaje es :" + mensaje;
-    }
+	@OnMessage
+	public String mensaje(String mensaje) {
+		System.out.println(mensaje);
+		return "hola desde el servidor el mensaje es :" + mensaje;
+	}
+
+	@OnOpen
+	public void open(Session session) {
+	}
+
+	@OnClose
+	public void close(Session session) {
+	}
+
+	@OnError
+	public void onError(Throwable error) {
+	}
 }
